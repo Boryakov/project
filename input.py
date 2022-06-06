a4a801b618adf1a8a76487425f381aea9cf45a0f
Boryakov <ltava.890@gmail.com>
2022-06-06 15:45:47 +0300

    #1 bugfix for input

--- input.py
+++ input.py
@@ -11,16 +11,26 @@ def cut(cur_list:list() ) -> dict():
         if cur_list[i].startswith("Вопрос: "):
             
             quest['question']=cur_list[i]
-            if cur_list[i+4].startswith("Прикрепленный файл:"):
-                quest['dir']="Директория вопроса: "+str(cur_list[i+4].split()[2])
-
+            try:
+                if cur_list[i+4].startswith("Прикрепленный файл:"):
+                    quest['dir']="Директория вопроса: "+str(cur_list[i+4].split()[2])
+            except:
+                pass
         elif cur_list[i].startswith("Ответ:"):
-            temp=cur_list[i+1].split()
+            index=i
+            while not cur_list[index].startswith("Приоритет:"):
+                if cur_list[i]!=cur_list[index]:
+                    cur_list[i]+=cur_list[index]
+                index+=1
+            temp=cur_list[index].split()
             if int(temp[1]) > 0:
                 a=dict()
-                if cur_list[i+2].startswith("Прикрепленный файл:"):
-                    a['answer']="Директория ответа: "+str(cur_list[i+2].split()[2])
-                else:
+                try:
+                    if cur_list[i+2].startswith("Прикрепленный файл:"):
+                        a['answer']="Директория ответа: "+str(cur_list[i+2].split()[2])
+                    else:
+                        a['answer'] = cur_list[i]
+                except:
                     a['answer'] = cur_list[i]
                 
                 a['priority'] = temp[1]
@@ -32,17 +42,17 @@ def cut(cur_list:list() ) -> dict():
     return cur_list 
 
 
-names = ["диоды.txt","конструктивные особенности дискретных элементов.txt",
-"лр-5(те,схем).txt","розрахунки в схемах з кодненсаторами.txt","транзистор.txt"]
-def lapota(name:str) -> list:
-    file = open(name,encoding="utf-8")
+def lapota(name:str) -> list and str:
+    file = open(name,encoding="ANSI")
     qa = list()
     cur = list()
-
+    title=''
 
 
 
     for num,line in enumerate(file):
+        if num==23:
+            title=line.strip()
         if num<27:
             continue  
         line = line.strip()
@@ -62,23 +72,5 @@ def lapota(name:str) -> list:
     cur=cut(cur)
     qa.append(cur)
 
-    return qa
+    return qa , title+'\n'
 
-""" qa=lapota(names[0])
-#print(qa)
-      
-test=open('test.txt','w',encoding='utf-8')
-test.write('huesos')
-for i in range(len(qa)):
-    qa[i]=dict(qa[i])
-    helpstr=str(i+1)+" "+str(qa[i]['question']['question'])
-    test.write(helpstr)
-    try:
-        test.write(qa[i]['question']['dir'])
-    except:
-        pass
-    for q in range(len(qa[i]['answers'])):
-        test.write(qa[i]['answers'][q]['answer'])
-        helpstr2="Приоритет: "+str(qa[i]['answers'][q]['priority'])
-        test.write(helpstr2)
- """
