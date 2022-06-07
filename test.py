6e90aa4c903e869c2146989c5518074a965c3ecc
Boryakov <ltava.890@gmail.com>
2022-06-07 20:01:45 +0300

    blah-blah-blah

--- test.py
+++ test.py
@@ -1,29 +1,28 @@
-from input import lapota,cut
+from input import lapota, cut
 import os
 
-names = ["1.txt","2.txt","3.txt","4.txt","5.txt","6.txt"] 
 
+names = ["1.txt", "2.txt", "3.txt", "4.txt", "5.txt", "6.txt"]
 
 
 for name in names:
-    qa,title=lapota(name)    
-    dir='answers/complete_'+name
+    qa, title = lapota(name)
+    dir = 'answers/complete_'+name
     try:
-        test=open(dir,'w',encoding='utf-8')
+        test = open(dir, 'w', encoding='utf-8')
     except:
         os.mkdir("answers")
-        test=open(dir,'w',encoding='utf-8')
+        test = open(dir, 'w', encoding='utf-8')
     test.write(title)
     for i in range(len(qa)):
-        qa[i]=dict(qa[i])
-        helpstr=str(i+1)+" "+str(qa[i]['question']['question'])+'\n'
+        qa[i] = dict(qa[i])
+        helpstr = str(i+1)+" "+str(qa[i]['question']['question'])+'\n'
         test.write(helpstr)
         try:
             test.write(str(qa[i]['question']['dir']+'\n'))
         except:
             pass
         for q in range(len(qa[i]['answers'])):
-            test.write(str(qa[i]['answers'][q]['answer']+'\n'))
-            helpstr2="Приоритет: "+str(qa[i]['answers'][q]['priority']+'\n')
-            test.write(helpstr2) 
-
+            test.write(str(qa[i]['answers'][q]['answer'] +
+                       "   (приоритет " + str(qa[i]['answers'][q]['priority']) + ')\n'))
+        test.write('\n')
